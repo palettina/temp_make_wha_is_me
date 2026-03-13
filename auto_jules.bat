7ef123c3338be9877ff4f86c251bbcedb6f00f80
palettina <mail@palettina.art>
2026-03-14 07:16:21 +0900

    addition

--- auto_jules.bat
+++ auto_jules.bat
@@ -2,5 +2,5 @@ cd /d "%~dp0"
 git add -A
 git commit -m "addition"
 git push
-pwsh -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0auto_jules.ps1" %*
+pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0auto_jules.ps1" %*
 pause
