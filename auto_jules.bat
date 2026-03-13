8c5cc522761d7232fc6259cca8be2c5181bf62ab
palettina <mail@palettina.art>
2026-03-14 07:19:41 +0900

    fix(auto_jules): pwsh起動コマンドを統一

--- auto_jules.bat
+++ auto_jules.bat
@@ -2,5 +2,4 @@ cd /d "%~dp0"
 git add -A
 git commit -m "addition"
 git push
-pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0auto_jules.ps1" %*
-pause
+pwsh -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0auto_jules.ps1" %*
